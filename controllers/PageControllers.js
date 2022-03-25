5c8140cc690c61fb874771b4202be90771796cfa
Joseph <joseph.torres.yz@gmail.com>
2022-03-25 18:44:46 -0500

    Holla

--- controllers/PageControllers.js
+++ controllers/PageControllers.js
@@ -61,7 +61,7 @@ const vistaregister = async(req, res)=>{
         //const conn = pool.getConnection();
         const result = await db.pool.query('INSERT INTO tasks (description) values (?)', [Usuario]);
         res.send(result);
-        console.log(conn);
+        console.log(result);
         res.render("./login.ejs", {title: "login", layout: "./blanco.ejs"});
         //connection.query("INSERT INTO users SET ?", {Usuario:Usuario, Password:passwordHaash},async(error, results)=>{
         //    if(error){
